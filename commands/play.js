82e5329a327478c6f1f0ecbabca84a6d5c6cf638
mihailo17 <mihailomilojkovic@yahoo.com>
2023-11-18 11:06:42 +0100

    voice chat join

--- commands/play.js
+++ commands/play.js
@@ -6,7 +6,19 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
 		.setDescription('Replies with Pong!'),
-		async execute(interaction) {
-		await interaction.reply('Pong!');
+	async execute(interaction) {
+		const member = interaction.member;
+		const voiceChannel = member.voice.channel;
+
+		if (voiceChannel) {
+			await joinVoiceChannel({
+				channelId: voiceChannel.id,
+				guildId: guildId,
+				adapterCreator: interaction.guild.voiceAdapterCreator,
+			});
+			await interaction.reply('Joined the voice channel!');
+		} else {
+			await interaction.reply('You need to be in a voice channel to use this command!');
+		}
 	},
 };
