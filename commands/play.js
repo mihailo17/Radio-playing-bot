a7cb1b57c3440b8b15febc5ade4a3d2b20b40555
mihailo17 <mihailomilojkovic@yahoo.com>
2023-11-18 15:28:27 +0100

    Added options for the play command, and more radio options

--- commands/play.js
+++ commands/play.js
@@ -1,10 +1,22 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
 
+const radios = {
+	juzni: "https://stream.radios.rs:9038/;*.mp3",
+	80: "https://naxidigital-80s128ssl.streaming.rs:8042/;*.mp3",
+	TDI: "https://streaming.tdiradio.com/tdiradio.mp3",
+}
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('play')
-		.setDescription('Plays music from a local file'),
+		.setDescription('Plays music from a radio')
+		.addStringOption(option =>
+			option.setName('station')
+				.setDescription('Select a radio station')
+				.setRequired(true)
+				.addChoices({name: 'Juzni', value: 'juzni'}, {name: '80s', value: '80'}, {name: 'TDI', value: 'TDI'})
+		),
 	async execute(interaction) {
 		const member = interaction.member;
 		const voiceChannel = member.voice.channel;
@@ -13,20 +25,28 @@ module.exports = {
 			await interaction.reply('You need to be in a voice channel to use this command!');
 			return;
 		}
+		
+		const station = interaction.options.getString('station');
+
+		const radioUrl = radios[station];
+		if (!radioUrl) {
+			await interaction.reply('Invalid radio station!');
+			return;
+		}
+
 		const player = createAudioPlayer();
-		const resource = createAudioResource('https://stream.radios.rs:9038/;*.mp3', {
-        inlineVolume: true
-    });
+		const resource = createAudioResource(radioUrl, {
+			inlineVolume: true
+		});
+
 		joinVoiceChannel({
 			channelId: voiceChannel.id,
 			guildId: voiceChannel.guild.id,
 			adapterCreator: interaction.guild.voiceAdapterCreator,
 		}).subscribe(player);
-		// message.guild.me.voice.setRequestToSpeak(true);
-		// Create an audio player and play the local audio file
-		
+
 		player.play(resource);
 
-		await interaction.reply('Playing the local audio file!');
-	},
-};
+		await interaction.reply(`Playing radio station: ${station}`);
+		},
+		};
